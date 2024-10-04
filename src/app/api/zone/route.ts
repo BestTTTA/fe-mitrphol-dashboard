eff1c3f05144deeebb8f7b91216a756688e18187
BestTTTA <bst12546@gmail.com>
2024-10-04 15:54:22 +0700

    test docker

--- src/app/api/zone/route.ts
+++ src/app/api/zone/route.ts
@@ -1,36 +1,31 @@
 import { createClient } from "redis";
 import { NextResponse } from "next/server";
 
-// Helper function to create a timeout for fetch
 function timeout(ms: number) {
   return new Promise((_, reject) =>
     setTimeout(() => reject(new Error("Request timed out")), ms)
   );
 }
 
-// Fetch data from Redis or external API and cache it
 async function fetchData(zone: string) {
-  const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+  const redisUrl = process.env.REDIS_URL;
   const client = createClient({ url: redisUrl });
   const cacheKey = `${zone}_data`;
-  const fetchTimeout = 60000; // Set timeout to 5 seconds
+  const fetchTimeout = 30000; 
 
   try {
-    // Connect to Redis
     await client.connect();
 
-    // Try to retrieve data from Redis cache
     const cachedData = await client.get(cacheKey);
 
     if (cachedData) {
       return JSON.parse(cachedData);
     }
 
-    // Fetch data from external API with timeout
     const response = (await Promise.race([
       fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${zone}`, { cache: 'no-store' }),
       timeout(fetchTimeout),
-    ])) as Response; // <-- Type assertion
+    ])) as Response; 
 
     if (!response.ok) {
       throw new Error(`Failed to fetch data from external API: ${response.statusText}`);
@@ -38,7 +33,6 @@ async function fetchData(zone: string) {
 
     const apiData = await response.json();
 
-    // Store the fetched data in Redis with expiration (86400 seconds = 24 hours)
     await client.setEx(cacheKey, 86400, JSON.stringify(apiData));
 
     return apiData;
@@ -46,13 +40,11 @@ async function fetchData(zone: string) {
     console.error("Error fetching data:", error);
     throw new Error(`Failed to fetch or cache data: ${error.message}`);
   } finally {
-    // Ensure Redis client disconnects properly
     await client.quit();
   }
 }
 
 
-// API Route handler
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const zone = searchParams.get('zone');
@@ -62,11 +54,9 @@ export async function GET(request: Request) {
   }
 
   try {
-    // Fetch data using the fetchData function
     const data = await fetchData(zone);
     return NextResponse.json(data);
   } catch (error: any) {
-    // Return error if fetching fails
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
